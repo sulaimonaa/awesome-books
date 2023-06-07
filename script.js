e5605e8d21359a04eb2486b2f1c8942cf7619a2e
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-06-07 10:54:39 +0100

    fix linters error

--- script.js
+++ script.js
@@ -12,46 +12,46 @@ class Bookstore {
       localStorage.setItem('books', JSON.stringify(this.bookLibrary));
     }
 
-  const store = localStorage.getItem('books');
+    const store = localStorage.getItem('books');
       if (store) {
-          this.bookLibrary = JSON.parse(store);
-    }
+        this.bookLibrary = JSON.parse(store);
+      }
   }
 
-createLibrary() {
-  this.BooksContainer.innerHTML = '';
-  this.bookLibrary.forEach((book, index) => {
-    const bookContainer = document.createElement('div');
-    bookContainer.innerHTML = `
-    <div class='book-list-container'>
-      <div class='wrap'>
-        <div>
-            <p>${book.titleBook}</p>
-            <p> ${book.authorBook}</p>
-        </div>
-        <div class='removeBtn'>
-          <button onclick="removeBook(${index})">Remove</button>
+  createLibrary() {
+    this.BooksContainer.innerHTML = '';
+    this.bookLibrary.forEach((book, index) => {
+      const bookContainer = document.createElement('div');
+      bookContainer.innerHTML = `
+      <div class='book-list-container'>
+        <div class='wrap'>
+          <div>
+              <p>${book.titleBook}</p>
+              <p> ${book.authorBook}</p>
+          </div>
+          <div class='removeBtn'>
+            <button onclick="removeBook(${index})">Remove</button>
+          </div>
         </div>
       </div>
-    </div>
-    `;
-    this.BooksContainer.appendChild(bookContainer);
-  });
+      `;
+      this.BooksContainer.appendChild(bookContainer);
+    });
   }
 
-addBook(e) {
-  e.preventDefault();
-  const titleBook = this.bookTitle.value;
-  const authorBook = this.bookAuthor.value;
-  const newBooks = { titleBook, authorBook };
+  addBook(e) {
+    e.preventDefault();
+    const titleBook = this.bookTitle.value;
+    const authorBook = this.bookAuthor.value;
+    const newBooks = { titleBook, authorBook };
 
-  this.bookLibrary.push(newBooks);
-  localStorage.setItem('books', JSON.stringify(this.bookLibrary));
+    this.bookLibrary.push(newBooks);
+    localStorage.setItem('books', JSON.stringify(this.bookLibrary));
 
-  this.createLibrary();
+    this.createLibrary();
 
-  this.bookTitle.value = '';
-  this.bookAuthor.value = '';
+    this.bookTitle.value = '';
+    this.bookAuthor.value = '';
   }
 }
 
@@ -70,4 +70,3 @@ form.addEventListener('submit', objectOne.addBook);
 objectOne.storedLocal();
 objectOne.createLibrary();
 removeBook();
-
