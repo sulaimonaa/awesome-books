c46f2982ca96d26fbdfc531884af3ece51fe9ac3
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-06-08 14:26:55 +0100

    fix eslint error

--- time.js
+++ time.js
@@ -1,31 +1,45 @@
 class DateFormatter {
-    constructor() {
-      this.months = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December',
-      ];
-    } 
-    getOrdinalSuffix(day) {
-      if (day >= 11 && day <= 13) {
-        return 'th';
-      } 
-      const lastDigit = day % 10;
-      switch (lastDigit) {
-        case 1: return 'st';
-        case 2: return 'nd';
-        case 3: return 'rd';
-        default: return 'th';
-      }
+  constructor() {
+    this.months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
   }
-    
+  getOrdinalSuffix(day) {
+    if (day >= 11 && day <= 13) {
+      return "th";
+    }
+    const lastDigit = day % 10;
+    switch (lastDigit) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  }
+
   formatTime(hours, minutes, seconds) {
-    const ampm = hours >= 12 ? 'pm' : 'am';
+    const ampm = hours >= 12 ? "pm" : "am";
     const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-    const formattedMinutes = minutes.toString().padStart(2, '0');
-    const formattedSeconds = seconds.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = seconds.toString().padStart(2, "0");
     return `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
   }
-    
+
   formatDate(date) {
     const month = this.months[date.getMonth()];
     const day = date.getDate();
@@ -33,7 +47,7 @@ class DateFormatter {
     const year = date.getFullYear();
     return `${month} ${day}${ordinalSuffix} ${year}`;
   }
-    
+
   formatDateTime(date) {
     const formattedDate = this.formatDate(date);
     const hours = date.getHours();
@@ -43,19 +57,19 @@ class DateFormatter {
     return `${formattedDate}, ${formattedTime}`;
   }
 }
-  
+
 // Example usage
 const formatter = new DateFormatter();
-const displayElement = document.querySelector('.dateDisplay');
-  
+const displayElement = document.querySelector(".dateDisplay");
+
 function updateDateTime() {
-const date = new Date();
-const formattedDateTime = formatter.formatDateTime(date);
-displayElement.textContent = formattedDateTime;
+  const date = new Date();
+  const formattedDateTime = formatter.formatDateTime(date);
+  displayElement.textContent = formattedDateTime;
 }
-  
+
 // Update the time immediately
 updateDateTime();
-  
+
 // Update the time every second
 setInterval(updateDateTime, 1000);
